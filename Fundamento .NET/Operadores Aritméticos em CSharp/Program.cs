44cc94f798da935269333c4f6e227dd4e3ca6980
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-30 09:38:00 -0300

    Operadores Aritméticos - concluído com sucesso!

---
+++ Fundamento .NET/Operadores Aritméticos em CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Operadores Aritméticos
+int a = 5;
+int b = 10;
+
+int c = a-b;
+int d = a*b;
+int e = a/b;
+int f = a+b;
+
+Console.WriteLine($"{a} - {b} = {c} \n {a} x {b} = {d} \n {a} / {b} = {e} \n {a} + {b} = {f}");
+
