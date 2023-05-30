8a8deae3d4a0fe8ff53df81fd4847a8b4c20cef4
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-30 10:56:24 -0300

    Menu Interativo - concluído com êxito!

--- Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Program.cs
+++ Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Program.cs
@@ -2,6 +2,8 @@
 
 using Conhecendo_as_Estruturas_de_Repetição_em_CSharp.Models;
 
+/*
+
 Console.WriteLine("=------- Estrutura FOR ------=");
 Tabuada testeTabFor = new Tabuada();
 testeTabFor.chamadaTabuadaFor(3);
@@ -20,3 +22,35 @@ if (num > 1000) {
     break;
 }
 } while(true);
+
+*/
+
+/* Construção de MENU - Interativo*/
+
+while(true) {
+    Console.WriteLine("Escolha sua opção: ");
+    Console.WriteLine("1 - Cadastrar novo Jogador");
+    Console.WriteLine("2 - Buscar Jogador");
+    Console.WriteLine("3 - Apagar Jogador");
+    Console.WriteLine("4 - Encerrar Jogo");
+    string opcao = Console.ReadLine();
+    switch(opcao) 
+    {
+        case "1":
+            Console.WriteLine("|--- Cadastro do Cliente ---|");
+            break;
+        case "2":
+            Console.WriteLine("|--- Buscar Jogador ---|");
+            break;
+        case "3":
+            Console.WriteLine("|--- Apagar Jogador ---|");
+            break;
+        case "4":
+            Console.WriteLine("Jogo Encerrado!");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Opção invalida!");
+            break;
+    }
+}
