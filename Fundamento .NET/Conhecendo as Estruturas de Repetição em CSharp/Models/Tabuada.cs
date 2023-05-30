72478a732161958e9478a410d1c4e43e31411abc
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-30 10:15:38 -0300

    Introdução em While em C# - concluída com êxito!

--- Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Models/Tabuada.cs
+++ Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Models/Tabuada.cs
@@ -7,10 +7,20 @@ namespace Conhecendo_as_Estruturas_de_Repetição_em_CSharp.Models
 {
     public class Tabuada
     {
-        public void chamadaTabuada(int valTab) {
+        public void chamadaTabuadaFor(int valTab) {
             Console.WriteLine("|---- Tabuada ----|");
             for (int c = 1; c <= 10; c++) {
                 Console.WriteLine($"{valTab} x {c} = {valTab*c}");
+            }
+            Console.WriteLine("|----- Fim -----|");
+        }
+
+        public void chamadaTabuadaWhile(int valTab) {
+            int cont = 1;
+            Console.WriteLine("|---- Tabuada ----|");
+            while (cont <= 10) {
+                Console.WriteLine($"{valTab} x {cont} = {valTab*cont}");
+                cont++;
             }
             Console.WriteLine("|----- Fim -----|");
         }
--- Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Program.cs
+++ Fundamento .NET/Conhecendo as Estruturas de Repetição em CSharp/Program.cs
@@ -3,6 +3,9 @@
 using Conhecendo_as_Estruturas_de_Repetição_em_CSharp.Models;
 
 Console.WriteLine("=------- Estrutura FOR ------=");
-Tabuada testeTab = new Tabuada();
-testeTab.chamadaTabuada(3);
+Tabuada testeTabFor = new Tabuada();
+testeTabFor.chamadaTabuadaFor(3);
 
+Console.WriteLine("=------ Esturura WHILE ------=");
+Tabuada testeTabWhile = new Tabuada();
+testeTabWhile.chamadaTabuadaWhile(7);
