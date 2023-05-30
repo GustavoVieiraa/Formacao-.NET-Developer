5b00c5aa87d86cd8f495e2eb59e2a4eb059457f2
Gustavo Vieira <gustavovieira292020@gmail.com>
2023-05-30 08:29:18 -0300

    Diferença entre Convert e Parse - Concluída com êxito!

--- Fundamento .NET/Tipos de Operadores em CSharp/Program.cs
+++ Fundamento .NET/Tipos de Operadores em CSharp/Program.cs
@@ -9,3 +9,18 @@ Console.WriteLine($"{a} + {b} = {c}");
 // c = c + 10;
 c += 10;
 Console.WriteLine($"9 + 10 = {c}");
+
+// Conversão de Int to String
+int gValor = Convert.ToInt32("24");
+gValor += 10;
+Console.WriteLine(gValor);
+
+// Conversão de Int to String - Parse
+int tValor = int.Parse("20");
+tValor += 20;
+Console.WriteLine(tValor);
+
+// Diferença entre o Parse e o Convert
+/*  Convert aceita valores Null para conversão
+    Parse não aceita valores Null para conversão.
+*/
